69870318936fd345037ae0e962ea923261189cd9
Gustavoo Farias Couto <farias.k1ng07@gmail.com>
2023-09-25 14:54:56 -0300

    Front-End

---
+++ JS_2SEM/aula_02 - EvenListener/Exemplo03.js
@@ -0,0 +1,15 @@
+//Atribuir valor dos elementos para a variavel
+const corSelecionada = document.querySelector("#corSelecionada");
+const exibirCor = document.querySelector("#exibirCor");
+
+//Funçao para atualizar a cor exibida, quando alterar a seleção
+function atualizarCor()
+{
+    const corSelecionadaValor = corSelecionada.value;
+    exibirCor.textContent = `Cor Selecionada; ${corSelecionada.value}`;
+    exibirCor.style.color = corSelecionadaValor;
+}
+
+//Adicionanda um ouvinte de evento change ao select
+
+corSelecionada.addEventListener("change", atualizarCor)
